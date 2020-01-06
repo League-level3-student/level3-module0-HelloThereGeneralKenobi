b2355f476d31c076de1501413d0c294d10941f4f
League Student <league@iMac-20.attlocal.net>
2020-01-05 17:27:10 -0800

    Finished array cheat sheet, worked on Robot Race. Presented Game

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -18,7 +18,7 @@ public class _00_ArrayCheatSheet {
 		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		for(int n = 0;n < strings.length - 1; n++) {
+		for(int n = 0;n <= strings.length - 1; n++) {
 			System.out.println(strings[n]);
 		}
 		//7. make an array of 50 integers
@@ -26,15 +26,33 @@ public class _00_ArrayCheatSheet {
 
 		//8. use a for loop to make every value of the integer array a random number
 		Random randGen = new Random();
-		for(int k = 0; k < fiddy.length - 1; k++) {
+		for(int k = 0; k <= fiddy.length - 1; k++) {
 			fiddy[k] = randGen.nextInt(50);
 		}
 		//9. without printing the entire array, print only the smallest number on the array
+		int smallest = 100;
+		for(int m = 0; m <= fiddy.length -1; m++) {
+			int num = fiddy[m];
+			if(fiddy[m] < smallest) {
+				smallest = fiddy[m];
+			}
+			
+		}
+		System.out.println(smallest);
 		//10 print the entire array to see if step 8 was correct
-
+		for(int o = 0; o < fiddy.length -1; o++) {
+			System.out.println(fiddy[o]);
+		}
 		//11. print the largest number in the array.
-		
+		int biggest = 0;
+		for(int x = 0; x <= fiddy.length -1; x++) {
+			int num2 = fiddy[x];
+			if(fiddy[x] > biggest) {
+				biggest = fiddy[x];
+			}
+			
+		}
 		//12. print only the last element in the array
-		
+		System.out.println(fiddy[48]);
 	}
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,11 +8,15 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
+		
+		
 		//2. create an array of 5 robots.
-
+		Robot[] robots;
 		//3. use a for loop to initialize the robots.
-
+		for(int i = 0; i < 4; i++) {
+			 robots = new Robot[5];
+		}
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 		//5. use another for loop to iterate through the array and make each robot move 
@@ -25,6 +29,6 @@ public class _01_RobotRace {
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
-
+	}
 	
 }
